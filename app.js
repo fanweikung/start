44ddf650774bd9accf42ce6492af4791798777cb
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-01-27 09:33:31 -0800

    Resolve app.js merge conflict

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ counter.inc()
 counter.dec()
 console.log(counter.getCount())
 
-/* destructure the functions; no need to preface */
 const {inc, dec, getCount} = require("./myModule")
 inc()
 inc()
 dec()
 console.log(getCount())
-
