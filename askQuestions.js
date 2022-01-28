8f5ed5a6d49c1411d71e093b16b4be5ecb0d2001
fanweikung <fan-wei.kung@unicodeconsulting.com>
2022-01-28 03:04:25 -0800

    Add emitter to write answers back to the terminal

--- askQuestions.js
+++ askQuestions.js
@@ -1,14 +1,25 @@
 const collectAnswers = require("./lib/collectAnswers")
 
 const questions = [
-    "What is your name?",
-    "Where do you live?",
-    "What are you going to do with node.js?"
+    "What is your name? ",
+    "Where do you live? ",
+    "What are you going to do with node.js? "
 ]
 
-collectAnswers(questions, answers => {
+const answerEvents = collectAnswers(questions, /*answers => {
     console.log("Thank you for your answers")
     console.log(answers)
-    process.exit()
+    process.exit()}*/
+)
 
+answerEvents.on("answer", answer => {
+    console.log(`your answer is : ${answer}`)
 })
+
+answerEvents.on("complete", answers => {
+    console.log("Thank you for your answers")
+    console.log(answers)
+    //process.exit()
+})
+
+answerEvents.on("complete", () => process.exit())
