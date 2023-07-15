4a50a30a54f724c9bbc671a86c9dda5756399138
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-15 10:31:40 -0700

    03_07 Added setInterval in start/

--- exercises/ch03/03_07/start/timers.js
+++ exercises/ch03/03_07/start/timers.js
@@ -4,3 +4,12 @@ console.log(`setting a ${waitTime / 1000} second delay`);
 const timerFinished = () => console.log("done");
 
 setTimeout(timerFinished, waitTime);
+
+const waitInterval = 500;
+let currentTime = 0;
+const incTime = () => {
+  currentTime += waitInterval;
+  console.log(`waiting ${currentTime / 1000} seconds`);
+};
+
+setInterval(incTime, waitInterval);
