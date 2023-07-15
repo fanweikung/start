12e9c2af0ab350f8800c9d0dc65c693555b20a3d
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-15 15:14:40 -0700

    04_01 Imported util.log to show date time stamp

--- exercises/ch04/04_01/start/core.js
+++ exercises/ch04/04_01/start/core.js
@@ -1,4 +1,7 @@
 var path = require("path");
+var util = require("util");
 
 const dirUpload = path.join(__dirname, "www", "files", "uploads");
 console.log(dirUpload);
+
+util.log(path.basename(__filename));
