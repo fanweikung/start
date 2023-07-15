e4e036c2a9403d1d4692a2b523c900c8c8d3ec57
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-15 15:36:38 -0700

    Imported v8 to call getHeapStatistics

--- exercises/ch04/04_01/start/core.js
+++ exercises/ch04/04_01/start/core.js
@@ -1,7 +1,10 @@
 var path = require("path");
 var util = require("util");
+var { getHeapStatistics } = require("v8");
 
 const dirUpload = path.join(__dirname, "www", "files", "uploads");
 console.log(dirUpload);
 
+util.log(dirUpload);
 util.log(path.basename(__filename));
+util.log(getHeapStatistics());
