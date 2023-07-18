960c2ded2b73ba3335aed835674d88eb628ccfd0
fanweikung <fan-wei.kung@unicodeconsulting.com>
2023-07-18 04:36:13 -0700

    06_02 start/ replaced process.stdin with readStream

--- exercises/ch06/06_02/start/writeStream.js
+++ exercises/ch06/06_02/start/writeStream.js
@@ -1,13 +1,19 @@
 const fs = require("fs");
 
 const writeStream = fs.createWriteStream("./assets/MyFile.txt", "utf-8");
-const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
+// const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
 
 // writeStream.write("Hello ");
 // writeStream.write("World\n");
+// process.stdout.write("Hello ");
+// process.stdout.write("World\n");
 
+process.stdin.on("data", (data) => {
+  writeStream.write(data);
+});
 // readStream.on("data", (data) => {
 //   writeStream.write(data);
 // });
 
-readStream.pipe(writeStream);
+process.stdin.pipe(writeStream);
+// readStream.pipe(writeStream);
